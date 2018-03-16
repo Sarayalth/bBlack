ca55b100e41f2390d2c5f5304d6ba93c97d62fb7
Sarayalth <sarayalth@gmail.com>
2018-03-16 14:29:45 +0100

    b

--- bBlack.plugin.js
+++ bBlack.plugin.js
@@ -1,25 +1,25 @@
 //META{"name":"bBlack"}*//
 
-var testPlugin = function () {};
+var bBlack = function () {};
 
-testPlugin.prototype.start = function () {
+bBlack.prototype.start = function () {
 javascript: (function() {
     $('head').append('<link rel="stylesheet" href="https://cdn.rawgit.com/Sarayalth/bBlack/master/bBlack.theme.css" type="text/css"/>');
 }());
 };
 
-testPlugin.prototype.getName = function () {
+bBlack.prototype.getName = function () {
     return "bBlack";
 };
 
-testPlugin.prototype.getDescription = function () {
+bBlack.prototype.getDescription = function () {
     return "bBlack is a REAL night mode for Discord";
 };
 
-testPlugin.prototype.getVersion = function () {
-    return "0.0.¯\_(ツ)_/¯";
+bBlack.prototype.getVersion = function () {
+    return "0.0.1";
 };
 
-testPlugin.prototype.getAuthor = function () {
+bBlack.prototype.getAuthor = function () {
     return "[S]";
 };
